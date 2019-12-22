b704d1fb8ee926b60810f9a50548ed2cd8ad3d9f
Bastien <bastien.ratat@gmail.com>
2019-12-22 19:44:54 +0800

    clean data, store it in a df, plot

--- stocks.py
+++ stocks.py
@@ -1,22 +1,28 @@
+"""
+Author : Bastien RATAT
+Scraping CAC40 thirty best stocks on Yahoo Finance
+"""
+
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import re
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 pattern_changes = '\+\d\.\d\d\%|\-\d\.\d\d\%'
-
 url = 'https://finance.yahoo.com/quote/%5EFCHI/components?p=%5EFCHI'
 driver_path = 'C:/chromedriver'
 driver = webdriver.Chrome(driver_path)
 driver.get(url)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-section = soup.find("section")
-
+# root tag for iterations
 tr = soup.find_all("tr", class_="BdT Bdc($c-fuji-grey-c) Ta(end) Fz(s)")
 
+# scraping ans cleaning variation data
 variation = []
 for element in tr:
     temp = re.findall(pattern_changes, element.text)
@@ -24,7 +30,6 @@ for element in tr:
 
 variation = [element[0] for element in variation]
 
-# clean dirty data in the list
 clean_variation = []
 for num in variation:
     if '+' in num:
@@ -32,13 +37,15 @@ for num in variation:
     elif '-' in num:
         clean_variation.append(float(num[:-1]))
 
-#clean_variation = [float(num[1:-1]) for num in variation]
-
+# scraping stocks symbols
 symbol = []
 for element in tr:
     symbol.append(element.find('td').find(
         'a', class_="C($c-fuji-blue-1-b) Cur(p) Td(n) Fw(500)").text)
 
+clean_symbol = [string[:-3] for string in symbol]
+
+# scraping stocks last prices
 prices = []
 for element in tr:
     temp = re.findall(r'\d+\.\d+', element.text)
@@ -46,14 +53,20 @@ for element in tr:
 
 prices = [float(num[0]) for num in prices]
 
+# dictionary of symbol, price variation and last price
 stock_dict = {
-    "symbol": symbol,
+    "symbol": clean_symbol,
     "price variation": clean_variation,
     "last prices": prices
 }
 
+# DataFrame in Pandas
 stocks_df = pd.DataFrame.from_dict(stock_dict)
-stocks_df.set_index('symbol')
 
-
-print(stocks_df.sort_values('price variation', ascending=False))
+# Plot datas using sns and plt
+sns.barplot(x='symbol', y='last prices', data=stocks_df, palette="Blues_d")
+plt.xticks(rotation=90)
+plt.xlabel('')
+plt.ylabel('PRICES')
+plt.title('CAC40 30 best stocks on Yahoo Finance')
+plt.savefig("cac40_top_stocks.png")
